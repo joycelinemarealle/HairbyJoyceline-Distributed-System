f8f6bea16e969c8cf21e5d55100b03891b77117c
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-09-12 11:54:37 -0400

    update customer entity

---
+++ src/main/java/com/hairbyjoyceline_marealle/hairbusiness/entity/Customer.java
@@ -0,0 +1,51 @@
+package com.hairbyjoyceline_marealle.hairbusiness.entity;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private int phoneNumber;
+
+    public Customer(int id, String name, String email, int phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Customer (){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
