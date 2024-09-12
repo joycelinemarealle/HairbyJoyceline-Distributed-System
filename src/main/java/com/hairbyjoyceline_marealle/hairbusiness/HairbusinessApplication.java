0057af55171e70d0a4a956dcac9dd64ba8cd94ff
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-09-12 11:19:39 -0400

    initial commit

---
+++ src/main/java/com/hairbyjoyceline_marealle/hairbusiness/HairbusinessApplication.java
@@ -0,0 +1,13 @@
+package com.hairbyjoyceline_marealle.hairbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairbusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairbusinessApplication.class, args);
+	}
+//
+}
---
+++ src/test/java/com/hairbyjoyceline_marealle/hairbusiness/HairbusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hairbyjoyceline_marealle.hairbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairbusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
