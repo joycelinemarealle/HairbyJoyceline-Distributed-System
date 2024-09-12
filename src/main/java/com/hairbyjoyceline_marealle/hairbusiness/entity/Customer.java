9c2c46fe7772e36b2366551492657095a25a6527
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-09-12 13:35:50 -0400

    entity customer class

--- src/main/java/com/hairbyjoyceline_marealle/hairbusiness/entity/Customer.java
+++ src/main/java/com/hairbyjoyceline_marealle/hairbusiness/entity/Customer.java
@@ -1,13 +1,31 @@
 package com.hairbyjoyceline_marealle.hairbusiness.entity;
 
-public class Customer {
-    private int id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customer_id;
+
+    @NotNull
     private String name;
     private String email;
     private int phoneNumber;
+    private final List<Appointment> appointments = new ArrayList<>()
 
-    public Customer(int id, String name, String email, int phoneNumber) {
-        this.id = id;
+    public Customer(int customer_id, String name, String email, int phoneNumber) {
+        this.customer_id = customer_id;
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
@@ -17,12 +35,12 @@ public class Customer {
 
     }
 
-    public int getId() {
-        return id;
+    public int getCustomer_id() {
+        return customer_id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
     }
 
     public String getName() {
@@ -47,5 +65,28 @@ public class Customer {
 
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return customer_id == customer.customer_id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(customer_id);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customer_id=" + customer_id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
     }
 }
