0a544cddec813b04154049b7dec7886604413303
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-09-30 14:48:12 -0400

    Controller for hairservice

--- src/main/java/com/hairbyjoyceline_marealle/hairbusiness/controller/HairServiceController.java
+++ src/main/java/com/hairbyjoyceline_marealle/hairbusiness/controller/HairServiceController.java
@@ -1,4 +1,40 @@
 package com.hairbyjoyceline_marealle.hairbusiness.controller;
 
+import com.hairbyjoyceline_marealle.hairbusiness.dto.HairServiceDTO;
+import com.hairbyjoyceline_marealle.hairbusiness.dto.HairServiceRequestDTO;
+import com.hairbyjoyceline_marealle.hairbusiness.service.HairServiceService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hairservice")
+@CrossOrigin
 public class HairServiceController {
+    private final HairServiceService hairServiceService;
+
+    public HairServiceController(HairServiceService hairServiceService) {
+        this.hairServiceService = hairServiceService;
+    }
+
+    @GetMapping
+    public List<HairServiceDTO> retrieveAllHairServices(){
+        return hairServiceService.retrieveAllHairServices();
+    }
+
+    @GetMapping("{hairService_id}")
+    public HairServiceDTO findHairServiceById(@PathVariable Long hairService_id){
+        return hairServiceService.findHairServiceById(hairService_id);
+    }
+
+    @GetMapping("/{hairService_id}")
+    public HairServiceDTO deleteHairService(@PathVariable Long hairService_id){
+        return hairServiceService.deleteHairService(hairService_id);
+    }
+
+    @PostMapping
+    public HairServiceDTO createHairService(@RequestBody HairServiceRequestDTO hairServiceRequestDTO){
+        return hairServiceService.createHairService(hairServiceRequestDTO);
+
+    }
 }
