7715953860bdcb8da9bc1608c56466a32c13f376
joy <joyceline.marealle@peopleshores.com>
2024-10-11 12:00:39 -0400

    routing to add details per hair style

--- src/app/hair-style/hair-style.component.ts
+++ src/app/hair-style/hair-style.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {HairStyle} from '../hair-style';
+import {RouterLink} from "@angular/router";
+import {RouterOutlet} from "@angular/router";
 
 
 @Component({
   selector: 'app-hair-style',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './hair-style.component.html',
   styleUrl: './hair-style.component.css'
 })
